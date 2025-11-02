5c6f406f20373750e784e533620731f60ed016a8
Samuel <ch9398@gmail.com>
2025-11-01 23:11:24 -0400

    redo build after the CMakeLists.txt is updated

---
+++ build/CMakeFiles/cuda_ai_inference.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/myunix/dev/cpp/cuda/cuda-ai-inference-starter/src/kernels.cu" "CMakeFiles/cuda_ai_inference.dir/src/kernels.cu.o" "gcc" "CMakeFiles/cuda_ai_inference.dir/src/kernels.cu.o.d"
+  "/home/myunix/dev/cpp/cuda/cuda-ai-inference-starter/src/main.cu" "CMakeFiles/cuda_ai_inference.dir/src/main.cu.o" "gcc" "CMakeFiles/cuda_ai_inference.dir/src/main.cu.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
