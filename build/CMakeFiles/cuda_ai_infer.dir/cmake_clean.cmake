cb0f0925ed8006839a68e34c2a794773fd7a6ff1
Samuel <ch9398@gmail.com>
2025-11-01 22:50:00 -0400

    inital commit

---
+++ build/CMakeFiles/cuda_ai_infer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_ai_infer.dir/src/kernels.cu.o"
+  "CMakeFiles/cuda_ai_infer.dir/src/kernels.cu.o.d"
+  "CMakeFiles/cuda_ai_infer.dir/src/main.cu.o"
+  "CMakeFiles/cuda_ai_infer.dir/src/main.cu.o.d"
+  "cuda_ai_infer"
+  "cuda_ai_infer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/cuda_ai_infer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
